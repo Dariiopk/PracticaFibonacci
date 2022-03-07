fd6389f0b6a5527afdaf726f71cd2fd684cabc33
Dariiopk <dariogarciamartin@uma.es>
2022-03-07 18:19:34 +0100

    Introducimos el codigo Fibonacci y la clase test vacía

---
+++ src/main/java/org/example/fibonacci/Fibonacci.java
@@ -0,0 +1,21 @@
+package org.example.fibonacci;
+
+public class Fibonacci {
+
+    public static int fibonacci(int n)
+    {
+        if (n>1){
+            return fibonacci(n-1) + fibonacci(n-2);  //función recursiva
+        }
+        else if (n==1) {  // caso base
+            return 1;
+        }
+        else if (n==0){  // caso base
+            return 0;
+        }
+        else{ //error
+            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            return -1;
+        }
+    }
+}
---
+++ src/test/java/org/example/fibonacci/FibonacciTest.java
@@ -0,0 +1,7 @@
+package org.example.fibonacci;
+
+import junit.framework.TestCase;
+
+public class FibonacciTest extends TestCase {
+
+}
