6d1e7794ba84c396401d04f91c38cd52fa53d911
Dariiopk <dariogarciamartin@uma.es>
2022-03-07 18:59:48 +0100

    Añadimos afterEach y beforeEach por comodidad, limpieza del código.

--- src/test/java/org/example/fibonacci/FibonacciTest.java
+++ src/test/java/org/example/fibonacci/FibonacciTest.java
@@ -1,7 +1,10 @@
 package org.example.fibonacci;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -17,11 +20,20 @@ import static org.junit.Assert.assertThrows;
 * Fibonacci -1 -> Error
 * */
 
+
+
 public class FibonacciTest {
+
+    public Fibonacci fibonacci;
+
+    @BeforeEach
+    private void setup() {fibonacci = new Fibonacci();}
+
+    @AfterEach
+    private void finish() {fibonacci = null;}
 
     @Test
     public void shouldReturn0IfNumberTheIs0 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 0;
         int obtainedValue = fibonacci.fibonacci(0);
         assertEquals(expectedValue, obtainedValue);
@@ -29,7 +41,6 @@ public class FibonacciTest {
 
     @Test
     public void shouldReturn1IfNumberTheIs1 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 1;
         int obtainedValue = fibonacci.fibonacci(1);
         assertEquals(expectedValue, obtainedValue);
@@ -37,7 +48,6 @@ public class FibonacciTest {
 
     @Test
     public void shouldReturn1IfNumberTheIs2 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 1;
         int obtainedValue = fibonacci.fibonacci(2);
         assertEquals(expectedValue, obtainedValue);
@@ -45,7 +55,6 @@ public class FibonacciTest {
 
     @Test
     public void shouldReturn2IfNumberTheIs3 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 2;
         int obtainedValue = fibonacci.fibonacci(3);
         assertEquals(expectedValue, obtainedValue);
@@ -53,7 +62,6 @@ public class FibonacciTest {
 
     @Test
     public void shouldReturn3IfNumberTheIs4 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 3;
         int obtainedValue = fibonacci.fibonacci(4);
         assertEquals(expectedValue, obtainedValue);
@@ -61,7 +69,6 @@ public class FibonacciTest {
 
     @Test
     public void shouldReturn75025IfNumberTheIs25 (){
-        Fibonacci fibonacci = new Fibonacci();
         int expectedValue = 75025;
         int obtainedValue = fibonacci.fibonacci(25);
         assertEquals(expectedValue, obtainedValue);
@@ -72,4 +79,5 @@ public class FibonacciTest {
         var fibonacci = new Fibonacci();
         assertThrows(RuntimeException.class, () -> fibonacci.fibonacci(-1));
     }
+
 }
